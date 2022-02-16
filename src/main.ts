121d5cebde46f2cc36e974017cd581b1d6419bf8
noahjamison <noahjamison17@gmail.com>
2022-02-15 20:40:32 -0700

    added global pipes and Logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const port = process.env.PORT || 3434;
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
+
 bootstrap();
