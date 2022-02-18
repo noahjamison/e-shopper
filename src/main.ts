384d3f0ee89e19f8d144576f1cd3c016f60da9b2
noahjamison <noahjamison17@gmail.com>
2022-02-17 23:15:37 -0700

    added some swagger decorators

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
   const port = process.env.PORT || 3434;
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Shopper API Contract')
+    .setDescription('This is the required output for the API')
+    .setVersion('1.0.0')
+    .addTag('product', "Representation of a seller's products")
+    .addTag('store', "Representation of a seller's storefront")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,17 +1,64 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from 'src/products/entities/product.entity';
+import {
+  ApiConsumes,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ApiBadRequestResponse } from '@nestjs/swagger/dist/decorators/api-response.decorator';
 
+@ApiTags('products')
+@ApiConsumes('application/json')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Returns a list of products',
+    operationId: 'getAllProducts',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful operation',
+    type: Product,
+    isArray: true,
+  })
+  @ApiNotFoundResponse({ status: 404, description: 'Products not found' })
   getAllProducts(): Promise<Product[]> {
     return this.productsService.findAllProducts();
   }
 
   @Get('/:productId')
+  @ApiOperation({
+    summary: 'Returns a single product',
+    operationId: 'getProductById',
+  })
+  @ApiParam({
+    name: 'productId',
+    required: true,
+    description: 'ID of product to return',
+    schema: { oneOf: [{ type: 'integer', format: 'int64' }] },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful operation',
+    type: Product,
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Invalid ID supplied',
+    type: Product,
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Product not found',
+    type: Product,
+  })
   getProductById(@Param('productId') productId: string): Promise<Product> {
     return this.productsService.findProductById(productId);
   }
